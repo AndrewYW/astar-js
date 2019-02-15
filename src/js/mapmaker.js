a6939fde776caca85caf80c8143f27296125d14a
AndrewYW <andrewwang39@gmail.com>
2019-02-15 14:58:14 -0500

    map generation works

--- src/js/mapmaker.js
+++ src/js/mapmaker.js
@@ -2,13 +2,21 @@ export const generateMap = () => {
   map = Array(160).fill().map(() => Array(160).fill('1'));
   setHards(map);
   setHighways(map);
+  console.table(map);
+  debugger;
   setBlocked(map);
-  setNodes(map);
-  return map;
+  console.table(map);
+  debugger;
+  // setNodes(map);
+  // return map;
 }
 
 const randomInt = ceiling => {
-  return Math.floor(Math.random() * (ceiling+1));
+  return Math.floor(Math.random() * (ceiling));
+}
+
+const randomBoolean = () => {
+  return (Math.random() < .5);
 }
 
 const euclidDistance = (xRow, xCol, yRow, yCol) => {
@@ -20,9 +28,6 @@ const euclidDistance = (xRow, xCol, yRow, yCol) => {
 
 const inBounds = (row, col, size) => {
   return ((row > -1) && (row < size) && (col > -1) && (col < size));
-}
-const randomBoolean = () => {
-  return (Math.random() < .5);
 }
 
 const setHards = map => {
@@ -47,18 +52,213 @@ const setHards = map => {
           }
         }
       }
+      count++;
     }
   }
 };
 
 const setHighways = map => {
+  var highways = [];
+  let tries = 0;
+  let paths = 0;
 
+  while(paths < 4) {
+    if (tries === 500) {
+      debugger;
+      highways = [];
+      tries = 0;
+    } else {
+      path = makePath(map, highways, setStartCoords(map.length));
+      if (path != null) {
+        highways = highways.concat(path);
+        paths++;
+      } else {
+        tries++;
+      }
+    }
+  }
+
+  highways.forEach(coord => {
+    if (map[coord.row][coord.col] === '1'){
+      map[coord.row][coord.col] = 'a';
+    } else {
+      map[coord.row][coord.col] = 'b';
+    }
+  });
 };
 
-const setBlocked = map => {
+const setStartCoords = (size) => {
+  let row = 0;
+  let col = 0;
+  let border = 0;
+  let setStart = false;
 
+  while(!setStart) {
+    if(randomBoolean()){
+      if(randomBoolean()){
+        row = 0;
+        col = randomInt(size);
+        border = 1;
+      } else {
+        row = size - 1;
+        col = randomInt(size);
+        border = 2;
+      }
+    } else {
+      if (randomBoolean()) {
+        row = randomInt(size);
+        col = 0;
+        border = 3;
+      } else {
+        row = randomInt(size);
+        col = size - 1;
+        border = 4;
+      }
+    }
+
+    if (!isCorner(row, col, size)) setStart = true;
+  }
+
+  return {
+    startCoords: {row, col},
+    border
+  };
+}
+
+const isCorner = (row, col, size) => {
+  return (
+  (row === size - 1 && col === size - 1) ||
+  (row === 0 && col === 0) || 
+  (row === 0 && col === size - 1) ||
+  (row === size - 1 && col === 0));
+}
+
+const makePath = (map, highways, { startCoords, border } ) => {
+  let path = [];
+  let row = startCoords.row;
+  let col = startCoords.col;
+  let dir = border;
+
+  let currentRow = row;
+  let currentCol = col;
+
+  while (true) {
+    for (let i = 0; i < 20; i++) {
+      let coords = {};
+      
+      switch (dir) {
+        case 1:
+          coords = {row: row+i, col};
+          currentRow = row+i;
+          break;
+        case 2:
+          coords = {row: row-i, col};
+          currentRow = row-i;
+          break;
+        case 3:
+          coords = { row, col: col + i };
+          currentCol = col + i;
+          break;
+        case 4:
+          coords = { row, col: col - i };
+          currentCol = col - i;
+          break;
+      }
+
+      if (!(highways.includes(coords) || path.includes(coords))) {
+        if (inBounds(coords.row, coords.col, map.length)) {
+          path.push(coords);
+        } else {
+          debugger;
+          return path.length > 99 ? path : null;
+        }
+      } else {
+        return null;
+      }
+    }
+
+    dir = changeDirection(dir);
+    switch(dir) {
+      case 1:
+        row = currentRow + 1;
+        col = currentCol;
+        break;
+      case 2: 
+        row = currentRow - 1;
+        col = currentCol;
+        break;
+      case 3:
+        row = currentRow;
+        col = currentCol + 1;
+        break;
+      case 4:
+        row = currentRow;
+        col = currentCol - 1;
+        break;
+    }
+  }
+}
+
+const changeDirection = dir => {
+  let newDir;
+  switch (dir) {
+    case 1:
+      if (Math.random < .6) {
+        newDir = 1;
+      } else if (randomBoolean()) {
+        newDir = 3;
+      } else {
+        newDir = 4;
+      }
+      break;
+    case 2:
+      if (Math.random < .6) {
+        newDir = 2;
+      } else if (randomBoolean()) {
+        newDir = 3;
+      } else {
+        newDir = 4;
+      }
+      break;
+    case 3:
+      if (Math.random < .6) {
+        newDir = 3;
+      } else if (randomBoolean()) {
+        newDir = 1;
+      } else {
+        newDir = 2;
+      }
+      break;
+    case 4:
+      if (Math.random < .6) {
+        newDir = 4;
+      } else if (randomBoolean()) {
+        newDir = 1;
+      } else {
+        newDir = 2;
+      }
+      break;
+  }
+
+  return newDir;
+};
+
+
+const setBlocked = map => {
+  const MAX_BLOCKED = Math.pow(map.length, 2) / 5;
+
+  let blocked = 0;
+  while (blocked != MAX_BLOCKED) {
+    let row = randomInt(map.length);
+    let col = randomInt(map.length);
+
+    if(map[row][col] === '1' || map[row][col] === '2') {
+      map[row][col] = '0';
+      blocked++;
+    }
+  }
 };
 
 const setNodes = map => {
 
-}
+};
