29ac53ed0b40514959f9f9e1b95b2d59c1a5914f
AndrewYW <andrewwang39@gmail.com>
2019-02-19 01:02:45 -0500

    finish travelCost (need to test)

--- src/js/node.js
+++ src/js/node.js
@@ -39,7 +39,89 @@ class Node {
 
 
   travelCost(targetNode) {
-    
+    var straight;
+
+    straight = (this.row === targetNode.row || this.col === targetNode.col) ? true : false;
+
+    if (straight) {
+      switch(this.type) {
+        case '1':
+          switch (targetNode.type) {
+            case '1':
+              return 1.0;
+            case '2':
+              return 1.5;
+            case 'a':
+              return 1.0;
+            case 'b':
+              return 1.5;
+          }
+          break;
+        case '2':
+          switch (targetNode.type) {
+            case '1':
+              return 1.5;
+            case '2':
+              return 2.0;
+            case 'a':
+              return 1.5;
+            case 'b':
+              return 2.0;
+          }
+          break;
+        case 'a':
+          switch (targetNode.type) {
+            case '1':
+              return 1.0;
+            case '2':
+              return 1.5;
+            case 'a':
+              return .25;
+            case 'b':
+              return .375;
+          }
+          break;
+        case 'b':
+          switch (targetNode.type) {
+            case '1':
+              return 1.5;
+            case '2':
+              return 2.0;
+            case 'a':
+              return .375;
+            case 'b':
+              return .25;
+          }
+          break;
+      }
+    } else {
+      switch (this.type) {
+        case '1':
+        case 'a':
+          switch (targetNode.type) {
+            case '1':
+            case 'a':
+              return Math.sqrt(2);
+            case '2':
+            case 'b':
+              return (Math.sqrt(2) + Math.sqrt(8)) / 2;
+          }
+          break;
+        case '2':
+        case 'b':
+          switch (targetNode.type) {
+            case '1':
+            case 'a':
+              return (Math.sqrt(2) + Math.sqrt(8)) / 2;
+            case '2':
+            case 'b':
+              return Math.sqrt(8);
+          }
+          break;
+      }
+    }
+
+    return 0;
   }
 }
 
